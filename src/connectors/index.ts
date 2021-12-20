dd6cc5f2fb09fa88830aaab168d5951344f67d8f
baldychristophe <baldychristophe@gmail.com>
2021-12-20 12:14:02 +0100

    Mistswap: compute pools worth and total net worth

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,28 +1,140 @@
 import { getDefaultProvider, Contract } from 'ethers'
-import { BigNumber } from 'ethers'
+import { BigNumber, utils } from 'ethers'
 
-const UniswapV2PairABI = require('../abi/UniswapV2Pair.json')
+const MasterChefV2ABI = require('../abi/MasterChefV2.json')
 const UniswapV2ERC20ABI = require('../abi/UniswapV2ERC20.json')
+const UniswapV2PairABI = require('../abi/UniswapV2Pair.json')
 
 const SMARTBCH_NODE_MAINNET = 'https://smartbch.fountainhead.cash/mainnet'
 
+const WBCH_ADDRESS = '0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04'
+const FLEXUSD_ADDRESS = '0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72'
+const MIST_ADDRESS = '0x5fA664f69c2A4A3ec94FaC3cBf7049BD9CA73129'
+
+const MISTSWAP_MASTERCHEF_ADDRESS = '0x3A7B9D0ed49a90712da4E087b17eE4Ac1375a5D4'
+const LPTOKEN_WBCH_FLEXUSD_ADDRESS = '0x24f011f12Ea45AfaDb1D4245bA15dCAB38B43D13'
+const LPTOKEN_MIST_FLEXUSD_ADDRESS = '0x437E444365aD9ed788e8f255c908bceAd5AEA645'
+
 export const provider = () => getDefaultProvider(SMARTBCH_NODE_MAINNET)
 
-export const getBalance = (address: string) : Promise<BigNumber> => {
-  return provider().getBalance(address)
+export const getBalance = (userAddress: string) : Promise<BigNumber> => {
+  return provider().getBalance(userAddress)
 }
 
 export const getBCHPriceUSD = async () => {
-  const LPTokenContractAddress = '0x24f011f12Ea45AfaDb1D4245bA15dCAB38B43D13'
-  const LPTokenContract = new Contract(LPTokenContractAddress, UniswapV2PairABI, provider())
-  const token0 = await LPTokenContract.token0()
-  const token1 = await LPTokenContract.token1()
+  const activeProvider = provider()
+  const BCHContract = new Contract(WBCH_ADDRESS, UniswapV2ERC20ABI, activeProvider)
+  const FLEXUSDContract = new Contract(FLEXUSD_ADDRESS, UniswapV2ERC20ABI, activeProvider)
 
-  const BCHContract = new Contract(token0, UniswapV2ERC20ABI, provider())
-  const balanceOfBCH = await BCHContract.balanceOf(LPTokenContractAddress)
-
-  const FLEXUSDContract = new Contract(token1, UniswapV2ERC20ABI, provider())
-  const balanceOfFlexUSD = await FLEXUSDContract.balanceOf(LPTokenContractAddress)
+  const [ balanceOfBCH, balanceOfFlexUSD ] = await Promise.all([
+    BCHContract.balanceOf(LPTOKEN_WBCH_FLEXUSD_ADDRESS),
+    FLEXUSDContract.balanceOf(LPTOKEN_WBCH_FLEXUSD_ADDRESS),
+  ])
 
   return balanceOfFlexUSD.div(balanceOfBCH)
+}
+
+export const getMISTPriceUSD = async () => {
+  const activeProvider = provider()
+  const MISTContract = new Contract(MIST_ADDRESS, UniswapV2ERC20ABI, activeProvider)
+  const FLEXUSDContract = new Contract(FLEXUSD_ADDRESS, UniswapV2ERC20ABI, activeProvider)
+
+  const [ balanceOfMIST, balanceOfFlexUSD ] = await Promise.all([
+    MISTContract.balanceOf(LPTOKEN_MIST_FLEXUSD_ADDRESS),
+    FLEXUSDContract.balanceOf(LPTOKEN_MIST_FLEXUSD_ADDRESS),
+  ])
+
+  return (Number(utils.formatEther(balanceOfFlexUSD)) / Number(utils.formatEther(balanceOfMIST))).toFixed(3)
+}
+
+export const getTokenPriceUSD = async (tokenAddress: string) => {
+  if (tokenAddress === WBCH_ADDRESS) {
+    return getBCHPriceUSD()
+  } else if (tokenAddress === MIST_ADDRESS) {
+    return getMISTPriceUSD()
+  } else if (tokenAddress === FLEXUSD_ADDRESS) {
+    return 1
+  }
+  // Default value
+  return 1
+}
+
+export const getActiveMistSwapPools = async (userAddress: string) => {
+  const activeProvider = provider()
+  const masterChefContract = new Contract(MISTSWAP_MASTERCHEF_ADDRESS, MasterChefV2ABI, activeProvider)
+  const poolLength = await masterChefContract.poolLength()
+
+  const poolInfos = await Promise.all(
+    Array.from({ length: poolLength }, (_, poolIndex) => masterChefContract.userInfo(poolIndex, userAddress))
+  )
+  return poolInfos
+  .map((poolInfo, index) => ({ index, poolInfo })) // Add the index
+  .filter(
+    (pool: { poolInfo: { amount: BigNumber, rewardDebt: BigNumber }, index: number }) => !pool.poolInfo.amount.isZero()
+  )
+}
+
+export const getPoolDetails = (poolIndex: number) => {
+  const activeProvider = provider()
+  const masterChefContract = new Contract(MISTSWAP_MASTERCHEF_ADDRESS, MasterChefV2ABI, activeProvider)
+  return masterChefContract.poolInfo(poolIndex)
+}
+
+export const getV2PairSummary = (poolAddress: string) => {
+  const activeProvider = provider()
+  const LPTokenPoolContract = new Contract(poolAddress, UniswapV2PairABI, activeProvider)
+  return Promise.all([
+    LPTokenPoolContract.name(),
+    LPTokenPoolContract.token0(),
+    LPTokenPoolContract.token1(),
+    LPTokenPoolContract.symbol(),
+    LPTokenPoolContract.totalSupply(),
+  ])
+}
+
+export const getMistSwapSummary = async (userAddress: string) => {
+  const activeProvider = provider()
+  const activePools = await getActiveMistSwapPools(userAddress)
+
+  const activePoolsWithDetails = await Promise.all(activePools.map(async (pool) => {
+    let poolDetails = await getPoolDetails(pool.index)
+    let [poolName, token0, token1, symbol, totalSupply] = await getV2PairSummary(poolDetails.lpToken)
+
+    const Token0Contract = new Contract(token0, UniswapV2ERC20ABI, activeProvider)
+    const Token1Contract = new Contract(token1, UniswapV2ERC20ABI, activeProvider)
+    const [
+      token0Name, token0Symbol, token0Balance,
+      token1Name, token1Symbol, token1Balance,
+    ] = await Promise.all([
+      Token0Contract.name(),
+      Token0Contract.symbol(),
+      Token0Contract.balanceOf(poolDetails.lpToken),
+
+      Token1Contract.name(),
+      Token1Contract.symbol(),
+      Token1Contract.balanceOf(poolDetails.lpToken),
+    ])
+
+    return {
+      ...pool,
+      poolDetails,
+      poolName,
+      symbol,
+      totalSupply,
+      token0: {
+        address: token0,
+        name: token0Name,
+        symbol: token0Symbol,
+        balance: pool.poolInfo.amount.mul(token0Balance).div(totalSupply),
+      },
+      token1: {
+        address: token1,
+        name: token1Name,
+        symbol: token1Symbol,
+        balance: pool.poolInfo.amount.mul(token1Balance).div(totalSupply),
+      },
+    }
+  }))
+  console.log(activePoolsWithDetails)
+  return activePoolsWithDetails
 }
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef, Dispatch, SetStateAction } from 'react'
 import { BigNumber, utils } from 'ethers'
 
-import { getBalance, getBCHPriceUSD } from '../connectors'
+import { getBalance, getTokenPriceUSD, getMistSwapSummary } from '../connectors'
 
 export const Portfolio = (props: {
   userWalletAddress: string,
@@ -9,17 +9,46 @@ export const Portfolio = (props: {
 }) => {
   const [balance, setBalance] = useState('')
   const [BCHPrice, setBCHPrice] = useState('')
+  const [MISTPrice, setMISTPrice] = useState('')
+  const [activePools, setActivePools] = useState(null as any)
 
   const inputRef = useRef(null as any)
 
-  const fetchBalance = () => {
-    getBalance(props.userWalletAddress).then((response: BigNumber) => setBalance(utils.formatEther(response)))
+  const getPrice = (tokenAddress: string) => {
+    if (tokenAddress === '0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04') {
+      return BCHPrice
+    } else if (tokenAddress === '0x5fA664f69c2A4A3ec94FaC3cBf7049BD9CA73129') {
+      return MISTPrice
+    }
+    return 1
+  }
+
+  const poolNetWorth = (pool: any) => {
+    return Number(
+      (Number(utils.formatEther(pool.token0.balance)) * Number(getPrice(pool.token0.address)))
+      +
+      (Number(utils.formatEther(pool.token1.balance)) * Number(getPrice(pool.token1.address)))
+    ).toFixed(2)
+  }
+
+  const totalPoolsNetWorth = (pools: any): number => {
+    return pools.reduce((acc: number, val: any) => acc + Number(poolNetWorth(val)), 0)
   }
 
   useEffect(() => {
-    fetchBalance()
-    getBCHPriceUSD().then((BCHprice: BigNumber) => setBCHPrice(BCHprice.toNumber().toFixed(2)))
-  })
+    getBalance(props.userWalletAddress).then((response: BigNumber) => setBalance(utils.formatEther(response)))
+    // WBCH
+    getTokenPriceUSD('0x3743eC0673453E5009310C727Ba4eaF7b3a1cc04').then(
+      (BCHprice: BigNumber) => setBCHPrice(BCHprice.toNumber().toFixed(2))
+    )
+    // MIST
+    getTokenPriceUSD('0x5fA664f69c2A4A3ec94FaC3cBf7049BD9CA73129').then(
+      (MISTPrice: string) => setMISTPrice(MISTPrice)
+    )
+
+    getMistSwapSummary(props.userWalletAddress).then((activePools) => setActivePools(activePools))
+
+  }, [props.userWalletAddress])
 
   return (
     <>
@@ -35,6 +64,7 @@ export const Portfolio = (props: {
               className="outline-none w-full px-3 rounded w-[26rem]"
               placeholder="Please enter your BEP20 wallet address"
               value={ props.userWalletAddress }
+              readOnly={ true }
             />
             <button
               className="text-white p-1 border border-white ml-3 material-icons rounded"
@@ -51,16 +81,50 @@ export const Portfolio = (props: {
       </div>
 
       <div className="w-full border border-sky-600 rounded p-4 mt-5 flex">
-        <div className="text-white text-center basis-3/12 flex-col">
+        <div className="text-white text-center basis-6/12 flex-col">
           <div className="text-xl farmwatch-gradient">Net Worth</div>
-          <div className="text-3xl">{ Number(Number(balance) * Number(BCHPrice)).toFixed(2) } $</div>
+          { BCHPrice && activePools &&
+            <div className="text-3xl">
+              ${ Number(
+                  (Number(balance) * Number(BCHPrice))
+                  +
+                  totalPoolsNetWorth(activePools)
+                ).toFixed(2)
+              }
+            </div>
+          }
         </div>
 
-        <div className="text-white text-center basis-3/12 flex-col justify-center">
+        <div className="text-white text-center basis-6/12 flex-col justify-center">
           <div className="text-xl farmwatch-gradient">Wallet Balance</div>
-          <div className="text-3xl">{ Number(balance).toFixed(4) } BCH</div>
+          <div className="text-3xl">${ Number(Number(balance) * Number(BCHPrice)).toFixed(2) }</div>
         </div>
       </div>
+
+      { activePools &&
+        <div className="w-full border border-sky-600 rounded p-4 mt-5">
+          <div className="text-white text-2xl mb-4">
+            Mistswap
+          </div>
+          <div className="flex flex-row gap-4">
+            { activePools.map((pool: any) => (
+              <div key={ pool.index } className="basis-4/12 text-white border border-sky-600 p-4">
+                <div className="text-lg">{ pool.poolName }</div>
+                <div className="text-lg">{ pool.token0.name }/{ pool.token1.name }</div>
+                <div>${ poolNetWorth(pool) }</div>
+                <div className="text-gray-400 text-sm">
+                  { Number(utils.formatEther(pool.poolInfo.amount)).toFixed(2) } { pool.symbol }
+                </div>
+                <div>
+                  <span>{ Number(utils.formatEther(pool.token0.balance)).toFixed(3) } { pool.token0.symbol }</span>
+                  <span>/</span>
+                  <span>{ Number(utils.formatEther(pool.token1.balance)).toFixed(3) } { pool.token1.symbol }</span>
+                </div>
+              </div>
+            )) }
+          </div>
+        </div>
+      }
     </>
   )
 }
